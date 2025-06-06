6a02701f465e42ec77cedce7d4ce407a737130e1
Indrasis Datta <indrasis.datta@yahoo.co.in>
2025-06-06 08:34:59 +0530

    Langchain text summarization

---
+++ code-assistant/app.py
@@ -0,0 +1,36 @@
+import requests 
+import json
+import gradio as gr 
+
+url="http://localhost:11434/api/generate"
+
+headers={
+    'Content-Type':'application/json'
+}
+
+history = []
+
+def generate_response(prompt):
+    history.append(prompt)
+    final_prompt = "\n".join(history)
+    data = {
+        "model": "codechamp",
+        "prompt": final_prompt,
+        "stream": False
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        print("Success: ", response)
+        response = response.text 
+        data = json.loads(response)
+        return data['response']
+    else:
+        print("Error: ", response.text)
+
+interface = gr.Interface(
+    fn = generate_response,
+    inputs = gr.Textbox(lines=4, placeholder="Enter your prompt"),
+    outputs = "text"
+)
+interface.launch()
